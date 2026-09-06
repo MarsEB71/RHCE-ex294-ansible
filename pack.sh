ddab366c4fa4d92b56ce0f67adbc6eceab76dc3d
eric <ericbaloux@gmail.com>
2026-09-05 18:02:41 -0700

    pack.sh added

---
+++ pack.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ansible all -m yum_repository -a "name=baseos description='BaseOS local' baseurl=https://reposerver.tdacentre.com/yum-repo/rhel-10-for-x86_64-baseos-rpms gpgkey=http://repo.mysql.com/RPM-GPG-KEY-mysql gpgcheck=0 enabled=1
+ansible all -m yum_repository -a "name=appstream description='AppStream local' baseurl=https://reposerver.tdacentre.com/yum-repo/rhel-10-for-x86_64-appstream-rpms gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release gpgcheck=0 enabled=1
+
+ 
